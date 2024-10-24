fb21dd0fd7b82a6243684c18c22fdc9cdb5aef84
GiovanniMariaLedda <giovannimarialedda01@gmail.com>
2024-10-24 17:30:47 +0200

    add ciclo for

--- js/script.js
+++ js/script.js
@@ -22,35 +22,42 @@ console.log('JS OK');
 //  
 // 
 
-//! Elaborazione 
-// Generatore1 di numeri casuali da 0 a 99
-const numbersGenerator1 = Math.random() * 99 ;
-// Generatore2 di numeri casuali da 0 a 99
-const numbersGenerator2 = Math.random() * 99 ;
-// Generatore3 di numeri casuali da 0 a 99
-const numbersGenerator3 = Math.random() * 99 ;
-// Generatore4 di numeri casuali da 0 a 99
-const numbersGenerator4 = Math.random() * 99 ;
-// Generatore5 di numeri casuali da 0 a 99
-const numbersGenerator5 = Math.random() * 99 ;
+// //! Elaborazione 
+// // Generatore1 di numeri casuali da 0 a 99
+// const numbersGenerator1 = Math.random() * 99 ;
+// // Generatore2 di numeri casuali da 0 a 99
+// const numbersGenerator2 = Math.random() * 99 ;
+// // Generatore3 di numeri casuali da 0 a 99
+// const numbersGenerator3 = Math.random() * 99 ;
+// // Generatore4 di numeri casuali da 0 a 99
+// const numbersGenerator4 = Math.random() * 99 ;
+// // Generatore5 di numeri casuali da 0 a 99
+// const numbersGenerator5 = Math.random() * 99 ;
 
-// variabile1 che arrotonda per eccesso
-const intNum1 = Math.ceil(numbersGenerator1);
-console.log(intNum1)
-// variabile2 che arrotonda per eccesso
-const intNum2 = Math.ceil(numbersGenerator2);
-console.log(intNum2)
-// variabile3 che arrotonda per eccesso
-const intNum3 = Math.ceil(numbersGenerator3);
-console.log(intNum3)
-// variabile4 che arrotonda per eccesso
-const intNum4 = Math.ceil(numbersGenerator4);
-console.log(intNum4)
-// variabile5 che arrotonda per eccesso
-const intNum5 = Math.ceil(numbersGenerator5);
-console.log(intNum5)
+// // variabile1 che arrotonda per eccesso
+// const intNum1 = Math.ceil(numbersGenerator1);
+// console.log(intNum1)
+// // variabile2 che arrotonda per eccesso
+// const intNum2 = Math.ceil(numbersGenerator2);
+// console.log(intNum2)
+// // variabile3 che arrotonda per eccesso
+// const intNum3 = Math.ceil(numbersGenerator3);
+// console.log(intNum3)
+// // variabile4 che arrotonda per eccesso
+// const intNum4 = Math.ceil(numbersGenerator4);
+// console.log(intNum4)
+// // variabile5 che arrotonda per eccesso
+// const intNum5 = Math.ceil(numbersGenerator5);
+// console.log(intNum5)
 
-// Prompt per chiedere i numeri all'utente 
-prompt('inserisci un numero da 0 a 99 ')
+// // Prompt per chiedere i numeri all'utente 
+// // prompt('inserisci un numero da 0 a 99 ')
 
-//! Output
+// Ciclo for
+for (let i = 0; i <5; i++){
+    const numbersGenerator = Math.random() * 99 ;
+    const intNum = Math.ceil(numbersGenerator);
+    console.log(intNum)   
+}
+
+// //! Output
